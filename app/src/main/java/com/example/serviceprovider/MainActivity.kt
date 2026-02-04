032b99b92bd3f23038d5bcdb6ed50003e844a70e
Sujal Shinde <sujalsunilshinde333@gmail.com>
2026-02-04 20:52:07 +0530

    Notification and User Profile

--- app/src/main/java/com/example/serviceprovider/MainActivity.kt
+++ app/src/main/java/com/example/serviceprovider/MainActivity.kt
@@ -5,23 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.serviceprovider.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        var NavController: NavController = findNavController(R.id.nav_host_fragment)
-        var bottomnav : BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView2)
-        bottomnav.setupWithNavController(NavController)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val bottomNav = binding.bottomNavigationView2
+        bottomNav.setupWithNavController(navController)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.notificationButton.setOnClickListener {
+            val bottomSheetFragment = Notification_Bottom_Fragment()
+            bottomSheetFragment.show(supportFragmentManager, "Test")
         }
     }
     //Sujal Shinde
---
+++ app/src/main/java/com/example/serviceprovider/Notification_Bottom_Fragment.kt
@@ -0,0 +1,42 @@
+package com.example.serviceprovider
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.serviceprovider.adapter.NotificationAdapter
+import com.example.serviceprovider.databinding.FragmentNotificationBottomBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class Notification_Bottom_Fragment : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentNotificationBottomBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentNotificationBottomBinding.inflate(layoutInflater,container,false)
+        val notifications = listOf("Your request has been Canceled Successfully","Your request has been accepted","Congrats Your request has been Placed")
+        val notificationImages = listOf(R.drawable.sademoji, R.drawable.truck,R.drawable.congrats)
+        val adapter = NotificationAdapter(
+            ArrayList(notifications),
+            ArrayList(notificationImages)
+        )
+        binding.notificationRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.notificationRecyclerView.adapter = adapter
+        return binding.root
+    }
+
+    companion object {
+
+    }
+}
